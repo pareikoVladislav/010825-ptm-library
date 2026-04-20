50238f01d5277b1c1cf95388539286be919a7743
Hallory <khalpeep@gmail.com>
2026-04-20 10:13:57 +0200

    add basic permission

--- core/settings.py
+++ core/settings.py
@@ -102,6 +102,12 @@ else:
     }
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
--- library/views/categories.py
+++ library/views/categories.py
@@ -1,23 +1,12 @@
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
 
 from library.models import Category
 from library.serializers import CategorySerializer
-
-"""## Задание 1. Category: список и создание через GenericAPIView + mixins
-
-### Эндпоинты:
-- `GET /api/v1/categories/`
-- `POST /api/v1/categories/`
-
-### Требования
-- Использовать `GenericAPIView` с миксинами `ListModelMixin` и `CreateModelMixin`.
-- Написать сериализатор `CategorySerializer` с полем `name`.
-
-### Проверки
-- `GET` возвращает список всех категорий (200).
-- `POST` с валидными данными создаёт категорию (201).
-- `POST` с дублирующимся `name` возвращает ошибку валидации (400), т.к. поле `unique=True`."""
-
+ 
+ 
 class CategoryListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    
+    authentication_classes = [BasicAuthentication]
